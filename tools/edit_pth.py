c61988a532372e7b8b5e75861c65847488cd2730
Cadoraghese <Cadoraghese@users.noreply.github.com>
2020-02-13 02:24:17 +0100

    Update edit_pth.py

--- tools/edit_pth.py
+++ tools/edit_pth.py
@@ -21,11 +21,11 @@ if __name__ == '__main__':
     params_dict = torch.load(args.input)
     params_dict['params'].train.dataset.path.source = 'data/datasets/open_image/general_400_32'
     params_dict['params'].train.dataset.path.target = 'data/datasets/open_image/general_400_256'
-    params_dict['params'].train.epochs = 275
+    params_dict['params'].train.epochs = 300
     #params_dict['params'].train.lr_schedule_patience = 2
     params_dict['params'].train.io.save_model_freq = 5
     params_dict['params'].train.io.eval_epoch_freq = 1
     params_dict['params'].train.io.print_errors_freq = 40
     params_dict['params'].data.scale = [8]
-    params_dict['params'].data.input_size = [24]
+    params_dict['params'].data.input_size = [18]
     torch.save(params_dict, args.output)
