6a33f2c3294a6118714320e189904c0e0f54b7ef
Cadoraghese <Cadoraghese@users.noreply.github.com>
2020-02-13 02:23:07 +0100

    L1_loss related files

--- tools/error_improvement_calculator.py
+++ tools/error_improvement_calculator.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def moving_average(rewards, T):
     array = np.zeros(T)
     window = 5
@@ -17,17 +18,19 @@ def moving_average(rewards, T):
     return rewards
 
 
-f = open("errors_files/gen_8x_32_256.txt", "r")
+name = 'gen_8x_long_with_low.txt'
+f = open("errors_files/"+name, "r")
 data_array = [[], [], []]
 
 for line in f:
-    if len(line.split(' ')) >= 7 and float(line.split(' ')[7]) != 0.0:
+    if len(line.split(' ')) >= 8 and float(line.split(' ')[7]) != 0.0:
         data_array[0] += [float(line.split(' ')[7])]
-    if len(line.split(' ')) >= 9 and float(line.split(' ')[9]) != 0.0:
+    if len(line.split(' ')) >= 10 and float(line.split(' ')[9]) != 0.0:
         data_array[1] += [float(line.split(' ')[9])]
-    if len(line.split(' ')) >= 11 and float(line.split(' ')[11]) != 0.0:
+    if len(line.split(' ')) >= 12 and float(line.split(' ')[11]) != 0.0:
         data_array[2] += [float(line.split(' ')[11])]
 
+plt.title(name)
 for data in data_array:
     plt.plot(moving_average(data, len(data)))
 plt.show()
